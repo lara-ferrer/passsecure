bd42be65ef14d17f83295f26489b3e9f28a007be
Lara Ferrer <79275240+lara-ferrer@users.noreply.github.com>
2021-12-12 23:19:46 +0100

    feat: add interactivity layer to the app and create requests to the API to fetch and send data (#1)
    * Add feature that allows users to delete sites
    
    * chore: add id to table
    
    * chore: change text in delete site link
    
    * chore: add icons
    
    * chore: modify homepage
    
    * chore: added interactivity layer
    
    * chore: added sites view
    
    * chore: modify styles
    
    * chore: render categories
    
    * chore: modify html structure
    
    * chore: add arrow in sidebar
    
    * chore: fetch categories and render sites related to its correspondent category
    
    * chore: add sites

---
+++ js/edit.js
@@ -0,0 +1,70 @@
+/**
+Edit sites
+**************************************/
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const siteId = urlParams.get('id');
+const categoryId = urlParams.get('category');
+
+if (siteId) {
+    fetch(`https://localhost:5001/Site/${siteId}`, {
+    method: 'GET'
+    }).then(response => {
+        return response.json()
+    }).then(data => 
+        editSite(data)
+    )
+}
+
+function editSite(response) {
+    let mainTitle = document.getElementsByClassName('site__title');
+    mainTitle.innerText = `Editar ${response.name}`;
+
+    let name = document.getElementById('name');
+    name.value = `${response.name}`;
+
+    let url = document.getElementById('url');
+    url.value = `${response.url}`;
+
+    let pass = document.getElementById('pass');
+    pass.value = `${response.password}`;
+
+    let desc = document.getElementById('desc');
+    desc.innerText = `${response.description}`;
+}
+
+
+/**
+Add sites
+**************************************/
+const form = document.getElementById("site-form");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+    const formData = new FormData(form);
+
+    var id = Math.floor(Math.random() * 100);
+    formData.append('id', id);
+
+    var date = new Date();
+    date.toLocaleDateString();
+    formData.append('creationDate', date);
+
+    formData.append('categoryId', categoryId);
+    
+    const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+    
+    fetch('https://localhost:5001/Site', {
+    method: 'POST',
+    body: formDataJsonString,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    }).then(response => {
+        return response.json()
+    }).then(response => 
+        window.location.href = "http://127.0.0.1:5500/web/"
+    )
+});
