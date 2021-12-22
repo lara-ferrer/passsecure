fa1cd1b56fb605669f28490fecc7178e8bf89401
Lara <larafpics@gmail.com>
2021-12-22 22:45:56 +0100

    fix: remove duplicated function

--- js/edit.js
+++ js/edit.js
@@ -91,22 +91,6 @@ function generatePass() {
     document.getElementById("pass").value = password;
 }
 
-/**
-Autogenerate secure password
-**************************************/
-function generatePass() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
-    var password = "";
-
-    for (var i = 0; i <= passwordLength; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber +1);
-    }
-
-    document.getElementById("pass").value = password;
-}
-
 
 /**
 Add sites
